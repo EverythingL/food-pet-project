64dcecf9a5330efea6d4eb41a50a8a7763903641
everything <75941260+EverythingL@users.noreply.github.com>
2022-07-21 10:08:26 +0300

    Added polyfills for support in older browsers

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@
 let path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './js/script.js',
   output: {
     filename: 'bundle.js',
@@ -15,7 +15,20 @@ module.exports = {
 
   module: {
     rules: [
-      {}
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', {
+                debug: true,
+                corejs: 3,
+                useBuiltIns: "usage"
+            }]]
+          }
+        }
+      }
     ]
   }
 };
